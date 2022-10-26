65a66fee813faa2d893637431fc263cf38494bf2
phongvu <62093198+phongvu@users.noreply.github.com>
2022-10-26 22:38:53 +0700

    add configure amplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+
+import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +34,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+  @override
+  initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    // Add any Amplify plugins you want to use
+    final authPlugin = AmplifyAuthCognito();
+    await Amplify.addPlugin(authPlugin);
+
+    // You can use addPlugins if you are going to be adding multiple plugins
+    // await Amplify.addPlugins([authPlugin, analyticsPlugin]);
+
+    // Once Plugins are added, configure Amplify
+    // Note: Amplify can only be configured once.
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on AmplifyAlreadyConfiguredException {
+      safePrint("Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
