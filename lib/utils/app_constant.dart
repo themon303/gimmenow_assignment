628113bcfa880b9fc24d10ec8eeb8ce786073f35
phongvu <62093198+phongvu@users.noreply.github.com>
2022-10-29 00:56:00 +0700

    hide test data

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,5 +1,3 @@
-import 'package:latlong2/latlong.dart';
-
 class AppConstants {
   static const String mapBoxAccessToken =
       'pk.eyJ1IjoicGhvbmd2dTE0IiwiYSI6ImNsOXI4a3p0NTE4NnYzcnA4bWZ6eHBhMmoifQ.F54Qaf5DPGqnU4g11GCawA';
@@ -9,6 +7,6 @@ class AppConstants {
 
   static const String mapBoxStyleId = 'cl9r8u4h4000015mvd5t4l3fr';
 
-  static final myLocation = LatLng(-37.886356265069146, 145.06682699972677);
-  static final insidePolygonPos = LatLng(-37.8868640534514, 145.08187974362951);
+  // static final myLocation = LatLng(-37.886356265069146, 145.06682699972677);
+  // static final insidePolygonPos = LatLng(-37.8868640534514, 145.08187974362951);
 }
